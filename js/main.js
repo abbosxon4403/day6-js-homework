44df00dd25e3e42396a5996e56bf728b2c6765dc
abbosxon <abbosxon.oripov@yandex.ru>
2022-04-05 18:09:53 +0500

    add homework

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let a = prompt("sonni kiriting , keyin esa uni 10 karagacha \n bo'lgan javobi console da chiqadi :)")
+for (i = 1; i <= 10; i++) {
+    console.log(a + ' x ' + i, ' = ', a * i);
+}
+console.log("end of counter :)")
